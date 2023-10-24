7f0aaa016e69a0d68294bf68e61b22583645b452
Mst Rinky Akter <mstrinkya@gmail.com>
2023-10-24 21:02:31 +0600

    ..

---
+++ index.js
@@ -0,0 +1,32 @@
+// variables 
+const generalBtn=document.getElementById('general');
+const businessBtn=document.getElementById('business');
+const sportsBtn=document.getElementById('sports');
+const entertainmentBtn=document.getElementById('entertainment');
+const technologyBtn=document.getElementById('technology');
+const searchBtn=document.getElementById('search');
+const newsQuery=document.getElementById('newsQuery');
+const newsType=document.getElementById('newsType');
+const newsDetails=document.getElementById('newsDetails');
+
+// apis 
+
+
+generalBtn.addEventListener('click',function(){
+
+});
+businessBtnlBtn.addEventListener('click',function(){
+
+});
+sportsBtn.addEventListener('click',function(){
+
+});
+entertainmentBtn.addEventListener('click',function(){
+
+});
+technologyBtn.addEventListener('click',function(){
+
+});
+searchBtn.addEventListener('click',function(){
+
+});
